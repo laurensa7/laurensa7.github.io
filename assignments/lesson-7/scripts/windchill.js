905165948b653ca45347b93906c498f0e689e1c1
lauren allred <laurenallred@rocketmail.com>
2018-11-22 21:52:11 -0500

    hi

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,30 +1,31 @@
 function windChill(tempf, speed) {
-			return 35.74 + 0.6215 + tempf - 35.75 * Math.power;
-		}
+	var f= 35.74 + 0.6215*tempf - 35.74 * Math.pow (speed,0.16)+
+		(0.4275*tempf*Math.pow(speed,0.16));
+	//Round to 3 Decimals
+	f = Math.round(f);
+	//Return value
+	return f;
+	
+		
+}
 
-		function compute() {
-			var temp = document.querySelector("windspeed");
-			windChill,
-			div = document.querySelector("output");
-			temp = parseFloat(temp);
-			speed = parseFloat(speed);
-			div.textContent = "feels like" + chilltofixed()
-		}
-		document.querySelector("cmd").onclick = compute;
-		<form>
-			<
-			label
-		for = "temperature" > Temperature( * F): < /label> <
-			input type = "text"
-		id = "temperature" >
-			<
-			label
-		for = "windspeed" > Wind Speed(MPH): < /label> <
-			input type = "text"
-		id = "windspeed" >
-			<
-			input type = "button"
-		value = "wind chill"
-		id = "cm" >
-			<
-			</form>
+function compute() {
+	var temp = document.querySelector("#temperature").value;
+	/*temp = parseFloat(temp);*/
+	console.log(temp)
+	var speed = document.querySelector("#windspeed").value;
+	console.log(speed)
+	
+	console.log(windChill(temp,speed));
+	
+	div = document.querySelector("#output");
+	div.value = "feels like " + windChill(temp,speed)
+}
+
+/*<form>
+<label for = "temperature"> Temperature( * F): </label> 
+<input type = "text" id = "temperature">
+<label for = "windspeed"> Wind Speed(MPH): </label> 
+<input type = "text" id = "windspeed">
+<input type = "button" value = "wind chill" id = "cm">
+</form>*/
