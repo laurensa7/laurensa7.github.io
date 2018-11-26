6259482d0620893c40497ec8e4a2828d8bfabc5d
lauren allred <laurenallred@rocketmail.com>
2018-11-25 22:36:05 -0500

    y

---
+++ assignments/lesson-9/scripts/windchill.js
@@ -0,0 +1,23 @@
+function windChill(tempf, speed) {
+	var f= 35.74 + 0.6215*tempf - 35.74 * Math.pow (speed,0.16)+
+		(0.4275*tempf*Math.pow(speed,0.16));
+	//Round to 3 Decimals
+	f = Math.round(f);
+	//Return value
+	return f;
+	
+		
+}
+
+function compute() {
+	var temp = document.querySelector("#temperature").value;
+	/*temp = parseFloat(temp);*/
+	console.log(temp)
+	var speed = document.querySelector("#windspeed").value;
+	console.log(speed)
+	
+	console.log(windChill(temp,speed));
+	
+	div = document.querySelector("#output");
+	div.value = "feels like " + windChill(temp,speed)
+}
