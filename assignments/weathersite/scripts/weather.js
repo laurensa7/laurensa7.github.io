cc703aa4527c9d0aaf52925445cc9927f2aabf7d
lauren allred <laurenallred@rocketmail.com>
2018-12-02 23:56:00 -0500

    h

---
+++ assignments/weathersite/scripts/weather.js
@@ -0,0 +1,17 @@
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=87a8f86b8f2c57a0b3b94458c45bd89e', true);
+
+weatherObject.send();
+
+weatherObject.onload = function () {
+	var weatherInfo = JSON.parse(weatherObject.responseText);
+	console.log(weatherInfo);
+
+
+	document.getElementById("place").innerHTML = weatherInfo.name;
+	document.getElementById("currentTemp").innerHTML = weatherInfo.main.temp;
+	document.getElementById("currentWind").innerHTML = weatherInfo.wind.speed;
+
+
+
+} // end of function
