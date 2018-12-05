a5479aec1196a87828c06b3712573924b370e720
lauren allred <laurenallred@rocketmail.com>
2018-12-04 21:17:20 -0500

    h

--- assignments/weathersite/scripts/weather.js
+++ assignments/weathersite/scripts/weather.js
@@ -1,5 +1,5 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Preston,Idaho,us&appid=87a8f86b8f2c57a0b3b94458c45bd89e&units=imperial', true);
+weatherObject.open('GET', '//api.openweathermap.org/data/2.5/weather?q=Preston,Idaho,us&appid=87a8f86b8f2c57a0b3b94458c45bd89e&units=imperial', true);
 
 weatherObject.send();
 
@@ -12,6 +12,9 @@ weatherObject.onload = function () {
 	document.getElementById("currentTemp").innerHTML = weatherInfo.main.temp;
 	document.getElementById("currentWind").innerHTML = weatherInfo.wind.speed;
 
+	var iconcode = weatherInfo.weather[0].icon;
+	var icon_path="//openweathermap.org/img/w/"+"50n"+".png";
+	document.getElementById('weather_icon').src= icon_path;
 
 
 } // end of function
